6b3f2cd0b597e0f395577d707bd46b12784d0252
Aleksandar Ristic <aristic665@gmail.com>
2021-04-18 03:06:02 +0200

    Second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Keramika Zlaja website
+
+## Site is made for a client who wanted to see 'many features' and movements, but also in a spirit of his business
+
+## Made with Sass, VanillaJs, Bootstrap, flaticon
+
+## OwlCarousel, easing, slick, aos
--- js/main.js
+++ js/main.js
@@ -94,32 +94,7 @@
         asNavFor: '.testimonial-slider'
     });
     $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
-    
-    
-    // Blogs carousel
-    $(".related-slider").owlCarousel({
-        autoplay: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-    
-    
+
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
